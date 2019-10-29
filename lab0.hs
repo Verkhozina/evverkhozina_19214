598a95db8c4adaa8837764f85c0ac46a682dc50e
Verkhozina <55305781+Verkhozina@users.noreply.github.com>
2019-10-29 19:24:07 +0700

    Update lab0.hs

--- lab0.hs
+++ lab0.hs
@@ -1,3 +1,10 @@
+ --Тут не хватает filter test и zip,
+ --потому что остались вопросы по их реализации.
+ 
+ get' :: [b] -> Integer -> b
+ get' (x:xs) 0 = x
+ get' (x:xs) n = get' xs (n-1)
+ 
  head' :: [a] -> a
  head' [] = error "The list is empty."
  head' (x:_) = x
@@ -15,45 +22,58 @@
  init' [] = error "The list is empty."
  init' [x] = []
  init' (x:xs) = x:(init xs)
- 
- reverse'' :: [a] -> [a] -> [a]
- reverse'' [] ys = ys
- reverse'' (x:xs) ys = reverse'' xs (x:ys)
 
- reverse' :: [a] -> [a]  
- reverse' [] = []
- reverse' xs = reverse'' xs []
+ reverse' :: [a] -> [a]
+ reverse' xs = reverse'' [] xs
+     where 
+         reverse'' ys [] = ys
+         reverse'' ys (x:xs) = reverse'' (x:ys) xs
  
- length' :: [a] -> Int
- length' [] = 0
- length' (x:xs) = 1 + length xs
+ length' :: [a] -> Integer
+ length' xs = length'' xs 0
+     where
+         length'' [] y = y
+         length'' (x:xs) y = length'' xs (y+1) 
+
+ append' :: [a] -> a -> [a]
+ append' [] y = [y]
+ append' (x:xs) y = x:append' xs y
+
+ concat' :: [a] -> [a] -> [a]
+ concat' [] ys = ys
+ concat' (x:xs) ys = x:concat' xs ys
+
+ drop' :: Integer -> [b] -> [b]
+ drop' 0 xs = xs
+ drop' _ [] = []
+ drop' n (x:xs) = drop' (n-1) xs
+
+ take' :: Integer -> [b] -> [b]
+ take' 0 xs = []
+ take' n (x:xs) = x:(take' (n-1) xs)
+ 
+ splitAt' :: Integer -> [b] -> ([b], [b])
+ splitAt' n xs = splitAt'' n [] xs
+     where
+         splitAt'' 0 ys xs = (ys, xs)
+         splitAt'' n ys (x:xs) = splitAt'' (n-1) (append' ys x) xs
  
  null' :: [a] -> Bool
  null' [] = True
  null' (_:_) = False
 
+ elem' :: Eq a => [a] -> a -> Bool
+ elem' xs y = elem'' 0 xs y
+     where
+         elem'' 0 [] y = False
+         elem'' 1 _ y = True
+         elem'' n (x:xs) y = if x == y then elem'' 1 xs y 
+                             else elem'' 0 xs y  
+
+ --filter test'
+
  map' :: (a -> b) -> [a] -> [b]
  map' f [] = []
  map' f (x:xs) = f x : map f xs
 
- append' :: [a] -> a -> [a]
- append' [] x = [x]
- append' xs x = reverse' (x:(reverse' xs))
-
- concat' :: [a] -> [a] -> [a]
- concat' xs [] = xs
- concat' xs (y:ys) = concat' (append' xs y) ys
-
- drop' :: (Eq a, Num a) => a -> [b] -> [b]
- drop' 0 xs = xs
- drop' _ [] = []
- drop' n (x:xs) = drop' (n-1) xs
-
- take'' :: (Eq a, Num a) => a -> [b] -> [b] -> [b]
- take'' 0 xs ys = ys
- take'' _ [] ys = ys
- take'' n (x:xs) ys = take'' (n-1) xs (x:ys)
-
- take' :: (Eq a, Num a) => a -> [b] -> [b]
- take' 0 xs = []
- take' n xs = reverse' (take'' n xs [])
+ --zip'
